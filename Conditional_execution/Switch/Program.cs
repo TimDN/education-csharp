5239ad9f0bce10a44dc3c3db77f127357ac701da
Tim Nielsen <tim.nielsen@codic.se>
2020-08-27 23:11:42 +0200

    Update switch example

---
+++ Conditional_execution/Switch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            switch (i)
+            {
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                case 3:
+                    Console.WriteLine("Two or Three");
+                    break;
+                default:
+                    Console.WriteLine("Other");
+                    break;
+            }
+        }
+    }
+}
