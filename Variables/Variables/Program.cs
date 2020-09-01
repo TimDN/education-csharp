e3beb376fe21fc1bbede0a5afbc2850adf0bd99b
Tim Nielsen <tim.nielsen@codic.se>
2020-09-01 21:11:53 +0200

    Update var example

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace Variables
+{
+    class Program
+    {
+#pragma warning disable CS0219 // Variable is assigned but its value is never used
+        static void Main(string[] args)
+        {
+            StringBuilder builder = new StringBuilder();
+            var stringB = new StringBuilder();
+            var age = 20;
+            var name = "Tim";
+        }
+    }
+#pragma warning restore CS0219 // Variable is assigned but its value is never used
+}
+
