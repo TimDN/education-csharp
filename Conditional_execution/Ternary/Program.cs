a8b10bc80ed69bc6fbbaac9ff0aa0531ff56ab95
Tim Nielsen <tim.nielsen@codic.se>
2020-10-22 17:25:39 +0200

    Update ternary

---
+++ Conditional_execution/Ternary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ternary();
+            IfElse();
+        }
+
+        private static void IfElse()
+        {
+            int x = 10, y = 100;
+
+            if (x > y)
+                Console.WriteLine("x is greater than y");
+            else
+                // will be printed
+                Console.WriteLine("x is less than y");
+        }
+
+        private static void Ternary()
+        {
+            int x = 10, y = 100;
+
+            var result = x > y ? "x is greater than y" : "x is less than y";
+
+            // x is less than y
+            Console.WriteLine(result);
+        }
+    }
+}
