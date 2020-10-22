32b3a76fae630d064a0d86cbda3db2785d97ab44
Tim Nielsen <tim.nielsen@codic.se>
2020-10-22 17:04:45 +0200

    Update methods

---
+++ MethodsDemo/Smallest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Smallest
+{
+    class Program
+    {
+        static int GetSmallest(int firstNumber, int secondNumber)
+        {
+            if(firstNumber < secondNumber)
+            {
+                return firstNumber;
+            }
+            else
+            {
+                return secondNumber;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] numbers = { 4, 5, 6 , 3 , 7 };
+
+            var smallest = int.MaxValue;
+
+            foreach(var number in numbers)
+            {
+                if(number < smallest)
+                {
+                    smallest = number;
+                }
+            }
+            Console.WriteLine(smallest);
+        }
+    }
+}
---
+++ MethodsDemo/swedish_bool_parser/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace swedish_bool_parser
+{
+    class Program
+    {
+        static bool TrySwedishBoolParser(string input)
+        {
+            if(input == "Ja" || input == "Sant")
+            {
+                return true;
+            }
+            else if(input == "Nej" || input == "Falskt")
+            {
+                return false;
+            }
+            else
+            {
+                throw new ArgumentException($"Error could not parse {input}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            try
+            {
+                var result = TrySwedishBoolParser(input);
+                Console.WriteLine(result);
+            }
+            catch (Exception e) when (e is ArgumentException || e is NullReferenceException)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
